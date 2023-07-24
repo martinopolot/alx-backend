891b9e072d00e33069151e4f6188b46b570173e7
martinopolot <maopolot@outlook.com>
2023-07-24 18:54:21 +0000

    Updated

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-""" Pagination main """
+"""Task 1: Simple pagination.
+"""
 
 import csv
 import math
 from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+    """
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
 
 
 class Server:
@@ -26,8 +34,13 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        pass
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
 
-    def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """ returns tuple of size two containing start index, end index """
-        pass
